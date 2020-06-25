423bda8aa4c0626957aa8e44a2a596e25f9b45bc
shaniel <shaniel_1@walla.com>
2020-06-25 14:40:53 +0300

    added cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 var express  = require('express');
+var cors  = require('cors');
 
 require('./db/mongoose');
 const User = require('./models/user');
@@ -9,6 +10,8 @@ const usersRouter = require('./routers/users');
 const utilsRouter = require('./routers/utils');
 
 var app = express();   
+app.use(cors());
+
 const port = process.env.PORT;
 
 app.use(express.static(__dirname + '/public'));   
@@ -16,6 +19,26 @@ app.use(express.json());
 app.use(recipesRouter);
 app.use(usersRouter);
 app.use(utilsRouter);
+
+// // Add headers
+// app.use(function (req, res, next) {
+
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 
 app.listen(port, ()=>{
